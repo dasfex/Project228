fb58fa7255cf9116ba50554ac7d85f7eb0019a49
KhodorVan <ixodor@mail.ru>
2019-03-19 16:03:19 +0300

    add herosprite files. Not end

---
+++ src/headers/herosprite.h
@@ -0,0 +1,12 @@
+#ifndef HEROSPRITE_H
+#define HEROSPRITE_H
+
+#include <QObject>
+
+class herosprite
+{
+public:
+    herosprite();
+};
+
+#endif // HEROSPRITE_H
---
+++ src/herosprite.cpp
@@ -0,0 +1,6 @@
+#include "herosprite.h"
+
+herosprite::herosprite()
+{
+
+}
