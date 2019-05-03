71c8813f5cca28d27df22214a794d2979443e93a
Daniil Grybchuk <renkens@famcs-steps.yaconnect.com>
2019-05-03 04:35:24 +0300

    new map

--- Project7/constants.h
+++ Project7/constants.h
@@ -1,9 +1,9 @@
 #ifndef PROJECT228_CONSTANTS_H
 #define PROJECT228_CONSTANTS_H
 
-const int MAP_WIDTH = 64;
-const int MAP_HEIGHT = 47;
-const int TILES_CNT = 45;
+const int MAP_WIDTH = 137; // 137
+const int MAP_HEIGHT = 89; // 89
+const int TILES_CNT = 56;
 const int TILE_SIZE = 64;
 
 const int HEROES_CNT = 3;
